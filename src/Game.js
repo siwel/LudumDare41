3849506c716c8bcd7f56c73a81779062e538015a
Yene Mulatu <yene.mulatu@blis.com>
2018-04-21 18:11:50 +0100

    "1 commit" change to PIXI.js

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,9 @@
-import 'pixi';
-import 'p2';
-import Phaser from 'phaser';
+import Demo from './states/Demo';
 
-import DrawSomethingState from './states/DrawSomethingState';
+export class Game {
+	constructor() {
 
-export class Game extends Phaser.Game {
-	constructor(gameContainer) {
-		const docElement = document.documentElement;
-		const width = docElement.clientWidth;
-		const height = docElement.clientHeight;
-
-		super(width, height, Phaser.AUTO, gameContainer);
-
-		this.state.add('DrawSomething', DrawSomethingState, false);
-		this.state.start('DrawSomething');
+		var demo = new Demo();
+		demo.init();
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-
-const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
-const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
-const creature = path.join(phaserModule, 'build/custom/phaser-creature.js');
-const pixi = path.join(phaserModule, 'build/custom/pixi.js');
-const p2 = path.join(phaserModule, 'build/custom/p2.js');
-
 const __DEV__ = process.env.NODE_ENV !== 'production';
 
 module.exports = {
 	entry: {
 		app: path.resolve(__dirname, 'src/app.js'),
-		vendor: ['pixi', 'p2', 'phaser']
 	},
 	output: {
 		filename: '[name].js',
@@ -36,10 +28,7 @@ module.exports = {
 						cacheDirectory: true
 					}
 				}, include: path.join(__dirname, 'src')
-			},
-			{ test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-			{ test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-			{ test: /p2\.js/, use: ['expose-loader?p2'] }
+			}
 		]
 	},
 	plugins: [
@@ -48,13 +37,6 @@ module.exports = {
 			template: './src/index.html'
 		})
 	],
-	resolve: {
-		alias: {
-			phaser,
-			pixi,
-			p2
-		}
-	},
 	devServer: {
 		contentBase: path.join(__dirname, 'build'),
 		compress: true,
