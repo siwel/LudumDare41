56ef9f29cea3c5e111ea19cd26e8f77c8196efe3
Cerden <cmincher@hotmail.co.uk>
2018-04-22 14:05:01 +0100

    Slight restructuring of Game

--- src/Game.js
+++ src/Game.js
@@ -9,58 +9,61 @@ import Recipes from "./Recipes";
 export default class Game {
     constructor () {
         this.activeIngredients = [];
+        this.belt = null;
+        this.gun = null;
+        this.restaurantManager = null;
     }
 
 	init () {
-        var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb});
-        document.body.appendChild(app.view);
+        this.app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb});
+        document.body.appendChild(this.app.view);
 
-        const belt = new ConveyerBelt(app.screen.width / 20, app.screen.height);
-        belt.sprite.interactive = true;
+        this.initBelt();
+        this.initGun();
 
-        belt.sprite.anchor.set(0.5);
-
-        belt.sprite.x = app.screen.width / 2;
-        belt.sprite.y = app.screen.height / 2;
-
-        const gun = new Gun(app.screen.height / 2);
-        
-        app.stage.addChild(belt.sprite);
-        app.stage.addChild(gun.sprite);
-
-        belt.setIngredientHeight(app.screen.height / 25);
-
-        app.stage.addChild(belt.sprite);
-
-
-        this.restaurantManager = new RestaurantManager(app.stage);
-
-        belt.start();
-
-        // Temp until we have working gun
-        belt.sprite.on('click', event => {
-            belt.registerHit(event.data.global.y);
-        })
-
-        setInterval(() => {
-            const newIngredient = this.getNextIngredient(app.screen.width / 25, app.screen.height / 25);
-
-            newIngredient.sprite.anchor.set(0.5);
-            newIngredient.sprite.x = app.screen.width / 2;
-            newIngredient.sprite.y = 0;
-
-            belt.addIngredient(newIngredient.sprite);
-
-            app.stage.addChild(newIngredient.sprite);
-        }, 1500);
+        this.restaurantManager = new RestaurantManager(this.app.stage);
 
         // Listen for animate update
-        app.ticker.add(delta => {
+        this.app.ticker.add(delta => {
             // just for fun, let's rotate mr rabbit a little
             // delta is 1 if running at 100% performance
             // creates frame-independent transformation
             // belt.rotation += 0.1 * delta;
         });
+    }
+
+    initBelt() {
+        this.belt = new ConveyerBelt(this.app.screen.width / 20, this.app.screen.height);
+        this.belt.sprite.interactive = true;
+
+        this.belt.sprite.x = this.app.screen.width / 2;
+        this.belt.sprite.y = this.app.screen.height / 2;
+        this.belt.setIngredientHeight(this.app.screen.height / 25);
+
+        this.app.stage.addChild(this.belt.sprite);
+
+        this.belt.start();
+
+        // Temp until we have working gun
+        this.belt.sprite.on('click', event => {
+            this.belt.registerHit(event.data.global.y);
+        })
+
+        setInterval(() => {
+            const newIngredient = this.getNextIngredient(this.app.screen.width / 25, this.app.screen.height / 25);
+
+            newIngredient.sprite.x = this.app.screen.width / 2;
+            newIngredient.sprite.y = 0;
+
+            this.belt.addIngredient(newIngredient.sprite);
+
+            this.app.stage.addChild(newIngredient.sprite);
+        }, 1500);
+    }
+
+    initGun() {
+        const gun = new Gun(this.app.screen.height / 2);
+        this.app.stage.addChild(gun.sprite);
     }
     
     getNextIngredient(width, height) {
