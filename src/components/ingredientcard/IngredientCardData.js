32ac84e7ade3fe318305ced29762ce746de751f8
Lewis Quaife <lewis@lewisquaife.com>
2018-04-22 16:39:28 +0100

    Switch Ingredient card View to use Ingredient class for its sprite

--- src/components/ingredientcard/IngredientCardData.js
+++ src/components/ingredientcard/IngredientCardData.js
@@ -6,16 +6,16 @@ export default class IngredientCardData {
     constructor(ingredients, location) {
 
         this.location = location;
-        this.plateView = new IngredientCardView(ingredients,this.location);
+        this.view = new IngredientCardView(ingredients,this.location);
         this.selectedIngredients = [];
     }
 
     getPlate () {
-        return this.plateView.getPlate();
+        return this.view.getPlate();
     }
 
     addIngredient (ingredient) {
-        this.plateView.addIngredient(ingredient);
+        this.view.addIngredient(ingredient);
         this.selectedIngredients.push(ingredient);
     }
 
