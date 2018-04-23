7116cc323f0a7d1ea7d7f359449b52675516f026
Cesar Zapata <cesar.zapata@blis.com>
2018-04-23 23:48:18 +0100

    Adds basic startup menu

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import Game from './Game';
 require('./styles/styles.css');
 
-const game = new Game();
-game.init();
+const startBtn = window.document.getElementById("start-btn");
+const introContainer = window.document.getElementById("intro");
+
+startBtn.onclick = function(){
+
+	introContainer.remove();
+	const game = new Game();
+	game.init();
+
+};
+
